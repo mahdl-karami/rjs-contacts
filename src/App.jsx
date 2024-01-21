fb058c396bcffbbd6ae83ae55b4a642c92a09e8f
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-01-21 12:41:13 +0330

    Create Validation & Styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Form from "./Components/Form";
 import Header from "./Components/Header";
+import Validation from "./Components/Validation";
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Form />
+			<Validation />
 		</>
 	);
 }
---
+++ src/Components/Validation.jsx
@@ -0,0 +1,8 @@
+import styles from "../Styles/Validation.module.css";
+export default function Validation() {
+	return (
+		<div className={styles.valid}>
+			<h3>Please Enter Valid Values</h3>
+		</div>
+	);
+}
