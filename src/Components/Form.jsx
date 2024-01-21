a439f5daf63a9c22b8eb6fc362432df69b3c905f
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-01-21 12:26:22 +0330

    Styled Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import inputs from "../Constant/inputs";
 import { changeHandler, submitHandler } from "../Helpers/handleForm";
+import styles from "../Styles/Form.module.css";
 export default function Form() {
 	const [values, setValues] = useState({
 		firstName: "",
@@ -9,7 +10,7 @@ export default function Form() {
 		number: "",
 	});
 	return (
-		<form onSubmit={(e) => submitHandler(e, values)}>
+		<form onSubmit={(e) => submitHandler(e, values)} className={styles.form}>
 			{inputs.map(({ name, type, placeHolder }) => (
 				<input key={name} value={values[name]} name={name} type={type} placeholder={placeHolder} onChange={(e) => changeHandler(e, setValues)}></input>
 			))}
