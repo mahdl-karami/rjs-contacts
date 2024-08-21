7f3c2728f5da6f49809e10bdb1cbbffa38ecb14f
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-21 17:19:40 +0330

    save states to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./Components/Form";
 import Header from "./Components/Header";
 import Validation from "./Components/Validation";
 import ContactsList from "./Components/ContactsList";
 
 function App() {
-	const [valid, setValid] = useState(true);
-	const [contacts, setContacts] = useState([]);
-	return (
-		<>
-			<Header />
-			<Form setValid={setValid} setContacts={setContacts} />
-			<Validation valid={valid}/>
-			<ContactsList contacts={contacts} setContacts={setContacts} />
-		</>
-	);
+  const [valid, setValid] = useState(true);
+  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem("contacts")) || []);
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+  return (
+    <>
+      <Header />
+      <Form setValid={setValid} setContacts={setContacts} />
+      <Validation valid={valid} />
+      <ContactsList contacts={contacts} setContacts={setContacts} />
+    </>
+  );
 }
 
 export default App;
