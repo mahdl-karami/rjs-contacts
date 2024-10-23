f661af9178a7179976662213aee995a1919b654b
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-10-23 13:09:45 +0330

    fix empty box responsive

--- src/Components/ContactsList.jsx
+++ src/Components/ContactsList.jsx
@@ -6,37 +6,39 @@ import { LuContact2 } from "react-icons/lu";
 import { removeHandler } from "../Helpers/handelContacts";
 
 export default function ContactsList({ contacts, setContacts }) {
-	return (
-		<ul className={styles.contacts}>
-			{contacts.length ? (
-				<>
-					{contacts.map(({ firstName, lastName, email, number, id }, i) => (
-						<li key={i}>
-							<span>
-								<LuContact2 />
-								{firstName} {lastName}
-							</span>
-							<span>
-								<HiOutlineMail />
-								{email}
-							</span>
-							<span>
-								<MdOutlineContactPhone /> {number}
-							</span>
-							<span>
-								<button onClick={() => removeHandler(id, contacts, setContacts)}>
-									<PiTrash />
-								</button>
-							</span>
-						</li>
-					))}
-				</>
-			) : (
-				<>
-					<h1>The Contact List Is Empty ;</h1>
-					<h3>Add New Contact ... </h3>
-				</>
-			)}
-		</ul>
-	);
+  return (
+    <ul className={styles.contacts}>
+      {contacts.length ? (
+        <>
+          {contacts.map(({ firstName, lastName, email, number, id }, i) => (
+            <li key={i}>
+              <span>
+                <LuContact2 />
+                {firstName} {lastName}
+              </span>
+              <span>
+                <HiOutlineMail />
+                {email}
+              </span>
+              <span>
+                <MdOutlineContactPhone /> {number}
+              </span>
+              <span>
+                <button onClick={() => removeHandler(id, contacts, setContacts)}>
+                  <PiTrash />
+                </button>
+              </span>
+            </li>
+          ))}
+        </>
+      ) : (
+        <>
+          <div className={styles.empty}>
+            <h1>The Contact List Is Empty ;</h1>
+            <h3>Add New Contact ... </h3>
+          </div>
+        </>
+      )}
+    </ul>
+  );
 }
